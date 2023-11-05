3cdd721dbe0355fa9bf3cc2fc1431664c7c9c5ea
Jorge Martin <jorgemctin@gmail.com>
2023-11-05 17:45:18 +0100

    Feat: Todo component

---
+++ src/components/Todo.tsx
@@ -0,0 +1,22 @@
+import { type Todo as TodoType } from '../types'
+
+type Props = TodoType
+
+export const Todo: React.FC<Props> = ({ id, title, completed }) => {
+    return (
+        <div className="view">
+            <input 
+            className="toggle"
+            checked={completed}
+            type="checkbox"
+            onChange={() => {}} 
+            />
+            <label>{title}</label>
+            <button
+            className='destroy'
+            onClick={() => {}}
+            >
+            </button>
+        </div>
+    )
+}
