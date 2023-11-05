243f2cdda1e786288208c5abd5264db815585387
Jorge Martin <jorgemctin@gmail.com>
2023-11-05 21:13:26 +0100

    Feat: Filters component

---
+++ src/components/Filters.tsx
@@ -0,0 +1,43 @@
+import { FiltersButton, type TodoFilters } from "../consts"
+import { filterValue } from "../types"
+
+
+interface Props {
+    onFilterChange: (filter: filterValue) => void
+    filterSelected: filterValue
+}
+
+export const Filters: React.FC<Props> = (
+    { filterSelected, onFilterChange }
+    ) =>{
+
+    // const handleClick = (filter: filterValue)
+    return(
+        <ul className="filters">
+            {
+            Object.entries(FiltersButton).map(( [key, {href, literal}] ) =>{
+                const isSelected = key  == filterSelected
+                const className = isSelected ? 'selected' : ''
+
+                return(
+                    <li key={key}>
+                        <a
+                        href={href}
+                        className={className}
+                        onClick={(e) => {
+                            e.preventDefault()
+                            onFilterChange(key as filterValue)
+                        }}
+                        >
+                        {literal}
+                        </a>
+                    </li>
+                )
+            })
+
+            }
+
+        </ul>
+    )
+}
+
